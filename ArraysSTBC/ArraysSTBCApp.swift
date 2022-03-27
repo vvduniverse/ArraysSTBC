41a27c98752246cbc8d2c27dc357f0be3e192adc
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-27 11:11:22 +0300

    Initial Commit

---
+++ ArraysSTBC/ArraysSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArraysSTBCApp.swift
+//  ArraysSTBC
+//
+//  Created by Vahtee Boo on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ArraysSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
